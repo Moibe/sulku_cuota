3343b858c1c6bd8d4283dee87afb5539b003011b
Moibe <moi.estrello@gmail.com>
2025-08-21 10:59:37 -0600

    Primer commit

---
+++ quotaCharger.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Reiniciando proceso
+cd
+cd chips/
+source venv/bin/activate
+cd sulku-quota
+
+# Ejecutar el script y capturar su salida
+quota_update=$(python updateQuota.py)
+
+timestamp=$(date +"%d-%m-%Y %H:%M:%S")
+
+# Guardar todo en una sola línea en el log
+echo "$timestamp - $quota_update"
