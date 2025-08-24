6ffa769e15ea742cbe1e44b68c8131a2857071b8
Moibe <moi.estrello@gmail.com>
2025-08-23 21:51:00 -0600

    Parámetro Iri @ 240

--- dailyRenew.py
+++ dailyRenew.py
@@ -4,5 +4,10 @@ import fireWhale
 if len(sys.argv) > 1:    
     parametro = sys.argv[1]
 
+if parametro == 'iri':
+    segundos = 240
+else: 
+    segundos = 1500
+
 fireWhale.editaDato("quota", parametro, "segundos", 1500)
 print(f"Servidor {parametro} actualizado.")
